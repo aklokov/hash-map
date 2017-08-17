81394ce1f0944cbfbf740e55793d906321ee19cf
repinvv <repin.vladv@gmail.com>
2017-08-17 23:58:49 +0300

    unneeded line

--- gulp/implementations/build-ts.js
+++ gulp/implementations/build-ts.js
@@ -2,7 +2,6 @@ const gulp = require('gulp');
 const ts = require('gulp-tsc');
 const settings = require('../settings');
 const tsconfig = require('../../tsconfig');
-const allTs = '/**/*.ts';
 
 gulp.task('build-ts', function () {
     const options = Object.assign({}, tsconfig.compilerOptions);
